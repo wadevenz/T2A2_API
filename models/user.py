f2efa2e2c3eaa36dc556368400b2e51f1ee50d72
[wadevenz] <[w.venz001@gmail.com]>
2024-07-15 14:05:51 +1000

    created controller for registering a user

--- models/user.py
+++ models/user.py
@@ -5,8 +5,8 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
 
