c3b071658abdf932758d5400a08830f70f7906c5
[wadevenz] <[w.venz001@gmail.com]>
2024-07-17 17:08:37 +1000

    location model and seed data

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -1,8 +1,12 @@
+from zoneinfo import ZoneInfo
+from datetime import datetime, timedelta
+
 from flask import Blueprint
 
 from init import db, bcrypt
 from models.user import User
 from models.team import Team
+from models.location import Location
 
 
 db_commands = Blueprint("db", __name__)
@@ -111,6 +115,99 @@ def seed_tables():
 
     db.session.add_all(teams)
 
+    locations = [
+        Location(
+            city="Brisbane, Queensland",
+            stadium="Gabba",
+            timezone=ZoneInfo("Queensland/Australia")
+        ),
+          Location(
+            city="Gold Coast, Queensland",
+            stadium="People First Stadium",
+            timezone=ZoneInfo("Queensland/Australia")
+        ),
+          Location(
+            city="Mount Barker, South Australia",
+            stadium="Adelaide Hills",
+            timezone=ZoneInfo("South/Australia")
+        ),
+          Location(
+            city="Adelaide, South Australia",
+            stadium="Adelaide Oval",
+            timezone=ZoneInfo("South/Australia")
+        ),
+          Location(
+            city="Hobart, Tasmaina",
+            stadium="Blundstone Arena",
+            timezone=ZoneInfo("Tasmania/Australia")
+        ),
+          Location(
+            city="Brisbane",
+            stadium="Gabba",
+            timezone=ZoneInfo("Queensland/Australia")
+        ),
+          Location(
+            city="Sydney, New South Wales",
+            stadium="ENGIE Stadium",
+            timezone=ZoneInfo("Sydney/Australia")
+        ),
+          Location(
+            city="Geelong, Victoria",
+            stadium="GMHBA Stadium",
+            timezone=ZoneInfo("Melbourne/Australia")
+        ),
+          Location(
+            city="Canberra, Australian Capital Territory",
+            stadium="Manuka Oval",
+            timezone=ZoneInfo("Sydney/Australia")
+        ),
+          Location(
+            city="Ballarat, Victoria",
+            stadium="Mars Stadium",
+            timezone=ZoneInfo("Victoria/Australia")
+        ),
+          Location(
+            city="Melbourne, Victoria",
+            stadium="Marvel Stadium",
+            timezone=ZoneInfo("Melbourne/Australia")
+        ),
+          Location(
+            city="Melbourne, Victoria",
+            stadium="MCG",
+            timezone=ZoneInfo("Melbourne/Australia")
+        ),
+          Location(
+            city="Norwood, South Australia",
+            stadium="Norwood Oval",
+            timezone=ZoneInfo("South/Australia")
+        ),
+          Location(
+            city="Perth, Australia",
+            stadium="Optus Stadium",
+            timezone=ZoneInfo("Perth/Australia")
+        ),
+          Location(
+            city="Sydney, New South Wales",
+            stadium="SCG",
+            timezone=ZoneInfo("Sydney/Australia")
+        ),
+          Location(
+            city="Darwin, Northern Territory",
+            stadium="TIO Stadium",
+            timezone=ZoneInfo("North/Australia")
+        ),
+          Location(
+            city="Alice Springs, Northern Territory",
+            stadium="TIO Traeger Park",
+            timezone=ZoneInfo("North/Australia")
+        ),
+          Location(
+            city="Launceston, Tasmania",
+            stadium="UTAS Stadium",
+            timezone=ZoneInfo("Tasmania/Australia")
+        ),
+    ]
+    db.session.add_all(locations)
 
     db.session.commit()
 
--- models/location.py
+++ models/location.py
@@ -1,5 +1,3 @@
-from zoneinfo import ZoneInfo
-from datetime import datetime, timedelta
 
 from init import db, ma
 
@@ -9,7 +7,7 @@ class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String, nullable=False)
     stadium = db.Column(db.String)
-    timezone = db.Column(db.Time)
+    timezone = db.Column(db.Date)
 
 class LocationSchema(ma.Schema):
     class Meta:
