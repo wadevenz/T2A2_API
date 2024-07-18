09c51776faa59f04bbd2d1088d8c41c65ba4cfa3
[wadevenz] <[w.venz001@gmail.com]>
2024-07-18 19:43:47 +1000

    relationship created between match team and tips models

--- models/match.py
+++ models/match.py
@@ -16,12 +16,13 @@ class Match(db.Model):
     winner = db.Column(db.String)
     
     location_id = db.Column(db.Integer, db.ForeignKey("locations.id"), nullable=False)
-    home_team = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable=False)
-    away_team = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable=False)
+    home_team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable=False)
+    away_team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable=False)
 
     locations = db.relationship("Location", back_populates="matches")
-    home_team = db.relationship("Team", back_populates="matches")
-    away_team = db.relationship("Team", back_populates="matches")
+    home_team = db.relationship("Team", foreign_keys=[home_team_id])
+    away_team = db.relationship("Team", foreign_keys=[away_team_id])
+    tips = db.relationship("Tip", back_populates="matches")
 
 class MatchSchema(ma.Schema):
 
--- models/team.py
+++ models/team.py
@@ -1,4 +1,5 @@
 from marshmallow import fields
+from sqlalchemy.orm import relationship
 
 from init import db, ma
 
@@ -9,10 +10,11 @@ class Team(db.Model):
     name = db.Column(db.String(100), nullable=False, unique=True)
     stadium = db.Column(db.String)
 
-    matches = db.relationship('Match', back_populates="teams")
+    matches = relationship('Match', primaryjoin="or_(Team.id==Match.home_team_id, Team.id==Match.away_team_id)", viewonly=True)
 
 class TeamSchema(ma.Schema):
-    matches = fields.Nested('MatchSchema', exclude=["teams"])
+
+    matches = fields.List(fields.Nested('MatchSchema', exclude=["teams"]))
 
     class Meta:
         fields = ("id", "name", "stadium", "matches")
